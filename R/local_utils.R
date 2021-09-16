bfa3d3c8c40ec60e52b09c1ce9d691686453ca3b
beyondpie <panshi.pie@gmail.com>
2021-09-16 14:51:16 -0700

    add load sparse matrix

--- R/local_utils.R
+++ R/local_utils.R
@@ -6,27 +6,29 @@
 #' @param cols vector[Integer]
 #' @param sep str
 #' @param ... others for read.csv
-#' 
+#'
 #' @return data.frame from read.csv
-#' 
+#'
 #' @export
 read_columns <- function(filenm, cols = c(1), sep = ",", ...) {
-  args <- c("-f", paste(cols, collapse = ","),
-           "-d", paste0("'", sep, "'"), filenm)
+  args <- c(
+    "-f", paste(cols, collapse = ","),
+    "-d", paste0("'", sep, "'"), filenm
+  )
   command <- "cut"
   command <- paste(c(shQuote(command), args), collapse = " ")
   tmp <- system(command, intern = TRUE)
-  invisible(utils::read.table(pipe(command), sep = sep,...))
+  invisible(utils::read.table(pipe(command), sep = sep, ...))
 }
 
 #' Load sparse matrix from mat file
 #'
 #' Each row of matfile: barcode[,genome_bin_index:counts]
-#' 
+#'
 #' @param matfile str
 #'
 #' @return sparseMatrix
-#' 
+#'
 #' @export
 load_sparse_matrix <- function(matfile) {
   lines <- data.table::fread(input = matfile, sep = "\n", header = FALSE)
@@ -40,7 +42,5 @@ load_sparse_matrix <- function(matfile) {
     r <- cbind(j, col_value)
   })
   r <- as.data.frame(do.call(what = rbind, args = row_col_value))
-  invisible(Matrix::sparseMatrix(i = r[,1], j = r[,2], x = r[, 3]))
+  invisible(Matrix::sparseMatrix(i = r[, 1], j = r[, 2], x = r[, 3]))
 }
-
-
