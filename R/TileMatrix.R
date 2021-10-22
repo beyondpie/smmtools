616055a0396b833fa154430febdae618689cdb3a
beyondpie <panshi.pie@gmail.com>
2021-10-22 16:55:59 -0700

    add tilematrix

--- R/TileMatrix.R
+++ R/TileMatrix.R
@@ -1,4 +1,21 @@
+#' Generate matrix of cell by genome bins for one sample
+#'
+#' Ref: ArchR
+#'
+#' @importFrom rhdf5 h5closeAll
 #' @export
-getTileMatrix <- function() {
+getTileMatrix <- function(rawH5File, outdir, outfilenm,
+                          genome = "mm10",
+                          sampleName = NULL, barcodes = NULL,
+                          nChunk = 3, tileSize = 5000,
+                          binarize = TRUE, excludeChr = c("chrM", "chrY"),
+                          threads = 1) {
+  tstart <- Sys.time()
+  message(paste("Begin to run getTileMatrix  with tileSize: ", tileSize, "at", tstart))
+  dir.create(outdir, showWarnings = FALSE, recursive = TRUE)
+  outfile <- file.path(outdir, outfilenm)
+  o <- h5closeAll()
+
+  annotGenome <- getAnnotFromArchRData(tag = "genome", genome = genome)
   return()
 }
