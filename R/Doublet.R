9c23e8c6d89b2a4c012189ea0f20d42cca643071
beyondpie <panshi.pie@gmail.com>
2021-10-31 14:53:40 -0700

    add doublet func

--- R/Doublet.R
+++ R/Doublet.R
@@ -1,5 +1,14 @@
 #' Doublet removement using Scrublet
-#' 
+#'
+#' Need smmutil 
+#' @importFrom reticulate r_to_py py_ro_r import
+#' @importFrom base nrow ncol
+#' @return list, five fields
+#'   1. threshold based on GMM for simulation scores [5]
+#'   2. threshold estimated from Scrublets
+#'   3. probs of real data based on GMM
+#'   4. doublet scores of real data based on Scrublets
+#'   5. doublet scores of simulation data based on Scrublets
 #' @export
 runScrublet <- function(bmat, path_to_python, expected_doublet_rate = 0.6,
                         min_counts = 3,
@@ -13,7 +22,22 @@ runScrublet <- function(bmat, path_to_python, expected_doublet_rate = 0.6,
   setSessionTimeLimit(cpu = Inf, elapsed = Inf)
 
   cat("Epoch: identify potential doublets ... \n")
-  out <- pass
+  scr <- import(module = "smmuty", covert = FALSE)
+  out <- src$detectDoublet(
+    counts_matrix = r_to_py(bmat),
+    expected_doublet_rate = expected_doublet_rate,
+    min_counts = min_counts,
+    min_cells = as.integer(min_cells),
+    min_gene_variability_pctl = min_conv_pctl,
+    n_prin_comps = min(n_pc, nrow(bmat), ncol(bmat)))
+  out <- py_to_r(out)
+  ## summary
+  cat("Epoch: summary doublets detection results ... \n")
+  message(paste("Threshold from GMM:", round(out[[1]], 3)))
+  message(paste("Threshold from Raw Scrublets", round(out[[2]], 3)))
+  message(paste("Number of cells used:"), length(out[[3]]))
+  message(paste("Doublet rate based on GMM:", round(length(which(out[[3]]>0.5)) / length(out[[3]]), 4)))
+  return(out)
 }
 
 
