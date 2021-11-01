ccb8b70d1ee6d271ef77bd24bc6ec0fde1d0427c
beyondpie <panshi.pie@gmail.com>
2021-10-31 22:54:11 -0700

    mannually add dependencies in smmuty

--- inst/smmuty/setup.py
+++ inst/smmuty/setup.py
@@ -17,5 +17,12 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires = [
+        "leidenalg",
+        "numpy",
+        "scrublet",
+        "sklearn",
+        "igraph",
+    ],
     python_requires=">=3.6",
 )
