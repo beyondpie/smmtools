652e3ceff0ca807623d66883176a060deb1196ea
beyondpie <panshi.pie@gmail.com>
2021-11-01 14:24:59 -0700

    add batchcorrection with Harmony

---
+++ R/BatchCorrection.R
@@ -0,0 +1,20 @@
+#' Perform batch effect using harmony
+#' 
+#' @param meta_data Either (1) Dataframe with variables to integrate or (2) vector with labels.
+#' @param vars_use If meta_data is dataframe, this defined which variable(s) to remove (character vector).
+#' @param ... Paramters passed to Harmony
+#' @return Matrix, cell by updated principle components
+#' @export
+SnapATAC_runHarmony <- function(mapSnapATAC, nPC, meta_data, vars_use = NULL, weightDimReduct = FALSE, ...) {
+  dmat <- mapSnapATAC$dmat
+  sdev <- mapSnapATAC$sdev
+  nCell <- nrow(dmat)
+  nDim <- ncol(dmat)
+  mat <- dmat[, 1:min(nPC, nDim)]
+  if(weightDimReduct) {
+    mat <- mat %*% sdev[1:min(nPC, nDim)]
+  }
+  harmonyEmbed <- harmony::HarmonyMatrix(data_mat = mat, meta_data = meta_data,
+                                         vars_use = vars_use, do_pca = FALSE, ...)
+  return(harmonyEmbed)
+}
