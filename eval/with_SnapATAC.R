509eeebbbaf7477b29556e8c95133abb62dcc2ff
beyondpie <panshi.pie@gmail.com>
2021-11-14 22:55:48 -0800

    minor

--- eval/with_SnapATAC.R
+++ eval/with_SnapATAC.R
@@ -1,14 +1,10 @@
-
-```{r}
 library(ggplot2)
 library(stringr)
 library(data.table)
 library(uwot)
-```
 
 ## analysis QC: nfragment and TSSE
 
-```
 qc_cancers <- read.table(file = "cancer_tissue_qc.tsv", header = TRUE,
                          quote = "", comment.char = "")
 qc_QY1287_Kai <- qc_cancers[qc_cancers$sample == "QY_1287",]
@@ -20,10 +16,9 @@ tsse_QY1287_smmtools <- read.table(file = "out/QY_1287_sumFragment.csv", header 
                                    quote = "", comment.char = "", sep = ",")
 rownames(tsse_QY1287_smmtools) <- tsse_QY1287_smmtools$barcode
 
-fragment_bam210x <- data.frame(tsse_QY1287_smmtools$nUniqFrag, qc_QY1287_SnapATAC[rownames(tsse_QY1287_smmtools), "uniq"])
+fragment_bam210x <- data.frame(tsse_QY1287_smmtools$nUniqFrag,qc_QY1287_SnapATAC[rownames(tsse_QY1287_smmtools), "uniq"])
 colnames(fragment_bam210x) <- c("smmtools", "SnapATAC")
 
 plot(x = log10(fragment_bam210x$smmtools), y = log10(fragment_bam210x$SnapATAC))
-```
 
 ## visualize the clustering result
