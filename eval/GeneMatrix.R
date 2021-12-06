b2430a4e23835b24d098ff57eb0124511a70b7f9
beyondpie <panshi.pie@gmail.com>
2021-12-06 13:38:21 -0800

    minor

--- eval/GeneMatrix.R
+++ eval/GeneMatrix.R
@@ -1,7 +1,12 @@
 ## get gene matrix for doublet removement
-library(smmtools)
-detach("package:smmtools", unload=TRUE)
+library(rtracklayer)
+## remotes::install_github("beyondpie/smmtools")
+## detach("package:smmtools", unload=TRUE)
 ## library(smmtools)
+
+## install.packages("plot3D")
+## install.packages("doSNOW")
+## remotes::install_github("r3fang/SnapATAC")
 library(SnapATAC)
 
 setwd("/mac/git-recipes/smmtools/eval")
@@ -15,14 +20,25 @@ sampleName <- "CEMBA201008_9F"
 genome <- "mm10"
 excludeChr <- c("chrM")
 
+## load geneup2k bed files
+geneUp2K <- rtracklayer::import(file.path("snap", "gencode.vM16.geneUp2k.bed"), format = "bed")
+genenms <- geneUp2K$name
+
 ## get barcodes filtered from smmtools
 sumFrag <- read.table(file = sumFragFile, header = TRUE, sep = ",", comment.char = "")
 index_from_nFrag <- which(sumFrag$nUniqFrag >= 1000)
 index_from_qc <- which(sumFrag$TSSE >= 10)
 index_join <- intersect(index_from_nFrag, index_from_qc)
-barcodes <- sumFrag[index_join]
+barcodes <- sumFrag[index_join,"barcode"]
 
+## load snap
+snap <- createSnap(file = snapFile, sample = sampleName, num.cores = 2)
+snap <- snap[which(snap@barcode %in% barcodes), ]
+snap <- SnapATAC::addGmatToSnap(obj = snap)
+
+## get gmat from smmtools
 gmat <- getGeneMatrix(rawH5File = rawH5File, outdir = outdir, outfilenm = outfilenm,
+                      genes = geneUp2K, genenms = genenms,
                       genome = genome, excludeChr = excludeChr, barcodes = barcodes)
 
 
