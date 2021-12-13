8b3b4155f4e255e4dce8128dc9e6123bf231bcca
beyondpie <panshi.pie@gmail.com>
2021-12-12 17:04:43 -0800

    minor

--- inst/rscript/getGmat.R
+++ inst/rscript/getGmat.R
@@ -7,22 +7,27 @@ option_list <- list(
   make_option(c("--outfnm"), type = "character"),
   make_option(c("--geneBedFile"), type = "character"),
   make_option(c("--excludeChrs"), type = "character", default = "chrM", help = "string seperated by comma"),
-  make_option(c("--genome"), type = "character", default = "mm10")
-  make_option(c("--barcodeFile"), type = "character", help = "no header, just one column of barcodes.")
+  make_option(c("--genome"), type = "character", default = "mm10"),
+  make_option(c("--barcodeFile"), type = "character", help = "no header, just one column of barcodes."),
+  make_option(c("--sampleName"), type = "character")
 )
 
 args <- parse_args(OptionParser(option_list = option_list))
 
+message("Load gene bed files.")
 genes <- rtracklayer::import(args$geneBedFile, format = "bed")
 genenms <- genes$name
+message("Load barcodes.")
 barcodes <- as.vector(read.table(file = args$barcodeFile, header = FALSE, comment.char = ""))
 excludeChr <- unlist(strsplit(args$excludeChr, split = ","))
 
-
+message("Generate Gene Matrix.")
 invisible(smmtools::getGeneMatrix(rawH5File = args$rawH5File,
                         outdir = args$outdir,
                         outfilenm = args$outfnm,
                         genes = genes, genenms = genenms,
                         genome = args$genome,
-                        excludeChr = excludeChr)
+                        excludeChr = excludeChr,
+                        sampleName = args$sampleName
+                        )
 
