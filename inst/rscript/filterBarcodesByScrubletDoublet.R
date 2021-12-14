ff05f88eed8a2eb70ea721bb9bedda064d03900d
beyondpie <panshi.pie@gmail.com>
2021-12-13 23:49:19 -0800

    add sampleName for filterScrubelt

--- inst/rscript/filterBarcodesByScrubletDoublet.R
+++ inst/rscript/filterBarcodesByScrubletDoublet.R
@@ -3,7 +3,8 @@ library(optparse)
 option_list <- list(
   make_option(c("--doubletScoreFile"), type = "character"),
   make_option(c("--outBarcodeFile"), type = "character"),
-  make_option(c("--outPlotFile"), type = "character")
+  make_option(c("--outPlotFile"), type = "character"),
+  make_option(c("--sampleName"), type = "character")
 )
 args <- parse_args(OptionParser(option_list = option_list))
 doubletScores <- read.table(file = args$doubletScoreFile, header = TRUE,
@@ -11,7 +12,7 @@ doubletScores <- read.table(file = args$doubletScoreFile, header = TRUE,
 
 pdf(file = args$outPlotFile, width = 10, height = 8)
 hist(doubletScores$scrubletScore, breaks = 50, xlab = "Scrublet Scores",
-     main = paste("Histogram of Scrublet scores for sample", sampleName))
+     main = paste("Histogram of Scrublet scores for sample", args$sampleName))
 abline(v = doubletScores$thresGMM[1], col = "red", lwd=3, lty=2)
 dev.off()
 
