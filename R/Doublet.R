b502c7553b9c4df1b37f368cf26a3dbab75f46fc
beyondpie <panshi.pie@gmail.com>
2021-12-26 09:56:13 -0800

    add normalMixEM in Doublet.R

--- R/Doublet.R
+++ R/Doublet.R
@@ -1,13 +1,23 @@
 #' Doublet removement using Scrublet
 #'
+#' GMM is used by Kai Zhang in his Cell ATAC Atlas paper.
+#' NormalMixEM is used by Yang Li in his CEMBA 1.0
+#'
 #' @param mat sparse matrix, cell by feature
+#' @param path_to_python string
+#' @param expected_doublet_rate double, default is 0.08
+#' @param min_counts numeric, default is 3
+#' @param min_cells integer, default is 5
+#' @param min_conv_pctl numeric, default is 85
+#' @param n_pc integer, default is 30
 #' @importFrom reticulate r_to_py py_to_r import
 #' @return list, five fields
-#'   1. threshold based on GMM for simulation scores [5]
-#'   2. threshold estimated from Scrublets
-#'   3. probs of real data based on GMM
-#'   4. doublet scores of real data based on Scrublets
-#'   5. doublet scores of simulation data based on Scrublets
+#'   - thresGMM: threshold based on GMM for simulation scores
+#'   - thresScrublet: threshold estimated from Scrublets
+#'   - probs: probs of real data based on GMM
+#'   - scrubletScore: doublet scores of real data based on Scrublets
+#'   - simuScrubletScore: doublet scores of simulation data based on Scrublets
+#'   - thresMixEM: threshold based normalmixEM for simulation scores.
 #' @export
 SnapATAC_runScrublet <- function(mat, path_to_python, expected_doublet_rate = 0.08,
                         min_counts = 3,
@@ -33,19 +43,53 @@ SnapATAC_runScrublet <- function(mat, path_to_python, expected_doublet_rate = 0.
   )
   out <- py_to_r(out)
   names(out) <- c("thresGMM", "thresScrublet", "probs", "scrubletScore", "simuScrubletScore")
-  ## summary
   scores <- out$scrubletScore
   ncell <- length(out$scrubletScore)
   tgmm <- out$thresGMM
   tscrub <- out$thresScrublet
+  tMixEM <- getScrubletThresholdByMixEM(scrubletSimScores = out$simuScrubletScore,
+                                        defaultCutoff = tscrub)
+  out$thresMixEM <- tMixEM
+  ## summary
   message("Epoch: summary doublets detection results ... \n")
   message(paste("Threshold from GMM:", round(tgmm, 3)))
   message(paste("Threshold from Raw Scrublets", round(tscrub, 3)))
   message(paste("Number of cells in total:"), ncell)
   message(paste("Doublet rate based on GMM:", round(sum(scores > tgmm) / ncell, 4)))
-  message(paste("Doublet rate based on RawScrublet:",
-                round(sum(scores > tscrub) / ncell, 4)))
+  message(paste("Doublet rate based on RawScrublet:",round(sum(scores > tscrub) / ncell, 4)))
+  message(paste("Doublet rate based on RawScrublet:", round(sum(scores > tMixEM) / ncell, 4)))
+  ## get result
   return(out)
 }
 
-## getScrubletThresholdByEM <- function()
+#' Another way to estimate the threshold for Scrublet
+#' Method used by Yang Li in CEMBA 1.0.
+#' @param scrubletSimScores numeric vector
+#' @param defaultCutoff double, if mixEM failed, use this as threschold.
+#' @param prob double, prob of a scrublet score treated as doublet, default is 0.5
+#' @param lower double, used for uniroot
+#' @return double 
+#' @importFrom mixtools normalmixEM
+#' @export
+getScrubletThresholdByMixEM <- function(scrubletSimScores, defaultCutoff,
+                                        prob = 0.5, lower = 0.2) {
+  x <- scrubletSimScores[scrubletSimScores > 0]
+  x <- x[is.finite(x)]
+  model <- normalmixEM(x, k = 2)
+  i <- which.min(model$mu)
+  f <- function(x) {
+    prob - (model$lambda[i]*dnorm(x, model$mu[i], model$sigma[i]) /
+               (model$lambda[1]*dnorm(x, model$mu[1], model$sigma[1]) +
+                  model$lambda[2]*dnorm(x, model$mu[2], model$sigma[2])))
+  }
+  ## How about using try-catch?
+  t <- try(stats::uniroot(f = f, lower = 0.2, upper = 1)$root, silent = TRUE)
+  if("try-error" %in% class(t)) {
+    cutoff <- defaultCutoff
+  } else {
+    cutoff <- stats::uniroot(f = f, lower = 0.2, upper = 1)$root
+  }
+  return(cutoff)
+}
+
+
