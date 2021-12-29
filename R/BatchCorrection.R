ee3fbd0e416ed204206c062a9d6d4a7921b727f8
beyondpie <panshi.pie@gmail.com>
2021-12-29 09:00:29 -0800

    add more descriptions for SnapATAC_runHarmony function

--- R/BatchCorrection.R
+++ R/BatchCorrection.R
@@ -1,10 +1,15 @@
 #' Perform batch effect using harmony
-#' 
+#'
+#' @param mapSnapATAC list, output of SnapATAC_runDiffusionMaps, at least contains
+#' - dmat matrix, cell by feature, dense matrix of embedding for the cells
+#' - sdev numeric vector, standard deviation from runDiffusionMaps.
+#' @param nPC integer, how many PCs used for Harmony, default is 30.
 #' @param meta_data Either (1) Dataframe with variables to integrate or (2) vector with labels.
 #' @param vars_use If meta_data is dataframe, this defined which variable(s) to remove (character vector).
 #' @return Matrix, cell by updated principle components
 #' @export
-SnapATAC_runHarmony <- function(mapSnapATAC, nPC, meta_data, vars_use = NULL, weightDimReduct = FALSE) {
+SnapATAC_runHarmony <- function(mapSnapATAC, nPC = 30,
+                                meta_data, vars_use = NULL, weightDimReduct = FALSE) {
   dmat <- mapSnapATAC$dmat
   sdev <- mapSnapATAC$sdev
   nCell <- nrow(dmat)
