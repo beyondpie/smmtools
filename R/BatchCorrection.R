b59544b13a0a3d7de22cd3797fb3755aed923fa8
beyondpie <panshi.pie@gmail.com>
2021-12-29 09:46:24 -0800

    minor: update document for runHarmony

--- R/BatchCorrection.R
+++ R/BatchCorrection.R
@@ -3,13 +3,17 @@
 #' @param mapSnapATAC list, output of SnapATAC_runDiffusionMaps, at least contains
 #' - dmat matrix, cell by feature, dense matrix of embedding for the cells
 #' - sdev numeric vector, standard deviation from runDiffusionMaps.
-#' @param nPC integer, how many PCs used for Harmony, default is 30.
 #' @param meta_data Either (1) Dataframe with variables to integrate or (2) vector with labels.
-#' @param vars_use If meta_data is dataframe, this defined which variable(s) to remove (character vector).
+#' @param nPC integer, how many PCs used for Harmony, default is 30.
+#' @param vars_use string, if meta_data is dataframe,
+#' this defined which variable(s) to remove (character vector).
+#' Default is NULL
+#' @param weightDimReduct bool, if weighted the PCs by multiplying sdev, default is FALSE
 #' @return Matrix, cell by updated principle components
 #' @export
-SnapATAC_runHarmony <- function(mapSnapATAC, nPC = 30,
-                                meta_data, vars_use = NULL, weightDimReduct = FALSE) {
+SnapATAC_runHarmony <- function(mapSnapATAC, meta_data,
+                                nPC = 30, vars_use = NULL,
+                                weightDimReduct = FALSE) {
   dmat <- mapSnapATAC$dmat
   sdev <- mapSnapATAC$sdev
   nCell <- nrow(dmat)
