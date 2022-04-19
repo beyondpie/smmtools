a6f55039c75deccbc818ba03ac8ea3015a5008e2
beyondpie <panshi.pie@gmail.com>
2022-04-18 23:01:33 -0700

    minor

--- R/IntegrateATACWithRNA.R
+++ R/IntegrateATACWithRNA.R
@@ -137,4 +137,73 @@ integrateWithScRNASeq <- function(snapSeurat,
   return(coEmbed)
 }
 
+getPredictId <- function(ovlp, atacLabel = "MajorType", coEmbed = coembedF) {
+  nms <- rownames(ovlp)
+  p1 <- vapply(nms, function(i) {
+    colnames(ovlp)[which.max(ovlp[i,])]
+  }, "")
+  p2 <- vapply(nms, function(i) {
+    pred <- coEmbed$predictId[coEmbed[[atacLabel]] %in% i]
+    stat <- table(pred)
+    names(stat)[which.max(stat)]
+  }, "")
+  data.frame(fromOvlp = p1, fromCoEmbed = p2,
+             row.names = nms)
+}
 
+
+# t1: table with 2 columns: coembed labels, raw labels
+# t2: table with 2 columns: coembed labels, raw labels
+cal_ovlpScore <- function(t1, t2) {
+  t1.table <- table(t1)
+  t2.table <- table(t2)
+  t1.pct <- apply(t1.table, 2, function(x) {
+    x / sum(x)
+  })
+  t2.pct <- apply(t2.table, 2, function(x) {
+    x / sum(x)
+  })
+  t1.labels <- colnames(t1.pct)
+  t2.labels <- colnames(t2.pct)
+  ovlpScore.df <- data.frame(
+    anno1 = as.character(),
+    anno2 = as.character(), ovlpScore = as.numeric()
+  )
+  for (t1.label in t1.labels) {
+    for (t2.label in t2.labels) {
+      t1.pct.df <- data.frame(t1.pct[, t1.label])
+      colnames(t1.pct.df) <- "t1"
+      t1.pct.df$ident <- rownames(t1.pct.df)
+      t2.pct.df <- data.frame(t2.pct[, t2.label])
+      colnames(t2.pct.df) <- "t2"
+      t2.pct.df$ident <- rownames(t2.pct.df)
+      comp.df <- join(t1.pct.df, t2.pct.df, by = "ident", type = "full")
+      comp.df[is.na(comp.df)] <- 0
+      comp.df$ident <- NULL
+      comp.df <- t(comp.df)
+      ovlpScore <- sum(apply(comp.df, 2, min))
+      out <- data.frame(anno1 = t1.label, anno2 = t2.label, ovlpScore = ovlpScore)
+      ovlpScore.df <- rbind(ovlpScore.df, out)
+    }
+  }
+  return(ovlpScore.df)
+}
+
+getOverlapMatrix <- function(metaF = coembedF, atacLabel = "MajorType", rnaLabel = "ClusterName") {
+  ident2rna <- data.frame(idents = metaF$coembed.idents, rna_label = metaF[[rnaLabel]])
+  ident2rna <- ident2rna[complete.cases(ident2rna), ]
+  ident2atac <- data.frame(idents = metaF$coembed.idents, atac_label = metaF[[atacLabel]])
+  ident2atac <- ident2atac[complete.cases(ident2atac), ]
+  ovlp <- cal_ovlpScore(ident2rna, ident2atac)
+  colnames(ovlp) <- c(rnaLabel, atacLabel, "ovlpScore")
+  ovlp <- reshape2::dcast(ovlp, as.formula(paste(atacLabel, "~", rnaLabel, sep = " ")),
+                          value.var = "ovlpScore", fun.aggregate =  identity, fill = 0.0)
+  ovlp <- as.data.frame(ovlp)
+  rnm <- ovlp[[atacLabel]]
+  rownames(ovlp) <- rnm
+  ## remove a name vector
+  ovlp[[atacLabel]] <- NULL
+  ovlp <- as.matrix(sapply(ovlp, as.numeric))
+  rownames(ovlp) <- rnm
+  return(ovlp)
+}
